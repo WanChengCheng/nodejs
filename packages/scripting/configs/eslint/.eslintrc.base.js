7ae39f00cc33181ef6d79405f7c69130318adfcf
Wan Cheng Cheng <chengcheng.st@gmail.com>
2019-01-30 16:33:20 +0800

    Add eslint to devDependencies in eslint-config.

---
+++ packages/scripting/configs/eslint/.eslintrc.base.js
@@ -0,0 +1,9 @@
+/*
+ * File: .eslintrc.base.js
+ * File Created: Wednesday, 30th January 2019 4:29:36 pm
+ * Author: ChegCheng Wan (chengcheng.st@gmail.com)
+ */
+
+module.exports = {
+  extends: "@chengchengw/eslint-config"
+};
