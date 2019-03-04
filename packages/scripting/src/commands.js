d81f77f32d168a18144693169d1aa0e3703f6f7f
Wan Cheng Cheng <chengcheng.st@gmail.com>
2019-03-04 20:58:27 +0800

    update scripting test scripts.

--- packages/scripting/src/commands.js
+++ packages/scripting/src/commands.js
@@ -9,6 +9,8 @@ const fs = require('fs-extra');
 const path = require('path');
 const { promisify } = require('util');
 const program = require('commander');
+const writePkg = require('write-pkg');
+const readPkg = require('read-pkg');
 const { exec } = require('./common/util');
 
 program.version('1.0.0');
@@ -29,36 +31,83 @@ program
       build, test, lint, git, editorconfig, sample,
     } = options;
     const copyFiles = [];
+    const scripts = {};
     if (build) {
+      //
+      // ──────────────────────────────────────────────────  ──────────
+      //   :::::: B U I L D : :  :   :    :     :        :          :
+      // ────────────────────────────────────────────────────────────
+      //
+
       copyFiles.push(['../configs/babel/babel.config.js', './babel.config.js']);
+      Object.assign(scripts, {
+        build: './node_modules/.bin/babel src --out-dir build --copy-files --source-maps',
+      });
+      logger.info('Adding babel build support ...');
+      await exec('yarn add -D @babel/cli');
+      await exec('yarn add -D  @babel/core');
+      await exec('yarn add -D  @babel/preset-env');
     }
     if (test) {
       if (test === 'ava') {
-        logger.info('Add ava test support...');
+        logger.info('Adding ava test support...');
         copyFiles.push(['../configs/ava/ava.config.js', './ava.config.js']);
         await exec('yarn add -D ava');
+        Object.assign(scripts, {
+          test: 'npm run build && ./node_modules/.bin/ava --verbose',
+          'test:watch': './node_modules/.bin/ava --verbose --watch',
+        });
       }
     }
     if (editorconfig) {
       copyFiles.push(['../configs/editor-config/.editorconfig', './.editorconfig']);
     }
     if (git) {
+      //
+      // ──────────────────────────────────────────────  ──────────
+      //   :::::: G I T : :  :   :    :     :        :          :
+      // ────────────────────────────────────────────────────────
+      //
+
       copyFiles.push(['../configs/git/.gitignore', './.gitignore']);
     }
     await Promise.all(
       copyFiles.map(([from, to]) => promisify(fs.copyFile)(path.join(__dirname, from), to)),
     );
     if (lint) {
+      //
+      // ────────────────────────────────────────────────  ──────────
+      //   :::::: L I N T : :  :   :    :     :        :          :
+      // ──────────────────────────────────────────────────────────
+      //
+
       // run yarn add -D @chengchengw/eslint-config &&  npx @chengchengw/eslint-config init
-      logger.info('Add lint configs...');
+      logger.info('Adding lint configs...');
       await exec('yarn add -D @chengchengw/eslint-config');
       logger.info('@chengchengw/eslint-config init ...');
       await exec('npx @chengchengw/eslint-config init');
     }
     if (sample) {
+      //
+      // ────────────────────────────────────────────────────  ──────────
+      //   :::::: S A M P L E : :  :   :    :     :        :          :
+      // ──────────────────────────────────────────────────────────────
+      //
+
       await fs.ensureDir('./src');
       await fs.copyFile(path.join(__dirname, '../configs/ava/main.js'), './src/main.js');
       await fs.copyFile(path.join(__dirname, '../configs/ava/main.test.js'), './src/main.test.js');
+    }
+    if (Object.values(scripts).length) {
+      logger.info('update package.json ...');
+      const pkg = await readPkg();
+      await writePkg({
+        ...pkg,
+        scripts: {
+          ...pkg.scripts,
+          ...scripts,
+        },
+      });
     }
     logger.info('done');
   });
