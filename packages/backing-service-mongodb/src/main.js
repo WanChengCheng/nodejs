e76e0e0bf171d4c00076a21c3627c68ef5dfc283
Wan Cheng Cheng <chengcheng.st@gmail.com>
2019-03-05 13:58:58 +0800

    Add default config, connector should be a HOF.

--- packages/backing-service-mongodb/src/main.js
+++ packages/backing-service-mongodb/src/main.js
@@ -3,13 +3,23 @@
  * File Created: Thursday, 28th February 2019 5:01:33 pm
  * Author: ChegCheng Wan <chengcheng.st@gmail.com>
  */
-import connect from './connect';
+import connectMongo from './connect';
 
 export const ServiceKey = 'resource/mongoose/mongo-connection';
 
-const connector = {
-  connect,
-  key: ServiceKey,
+export const DefaultMeta = {};
+
+export const DefaultConfig = {
+  nodes: JSON.parse(process.env.SERVICE_MONGO_NODES || null) || [],
+  replset: process.env.SERVICE_MONGO_REPLSET,
+  dbname: process.env.SERVICE_MONGO_DB_NAME,
+  user: process.env.SERVICE_MONGO_USERNAME,
+  pass: process.env.SERVICE_MONGO_PASSWORD,
 };
+
+const connector = (...meta) => (...config) => ({
+  key: ServiceKey,
+  connect: () => connectMongo(...meta)(...config),
+});
 
 export default connector;
