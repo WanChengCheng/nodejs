69139905af85cd92cdd59d32833e9065f54e97f5
Wan Cheng Cheng <chengcheng.st@gmail.com>
2019-03-06 16:45:18 +0800

    redis

--- packages/backing-service-redis/src/main.js
+++ packages/backing-service-redis/src/main.js
@@ -4,6 +4,21 @@
  * Author: ChegCheng Wan <chengcheng.st@gmail.com>
  */
 
-export const fun = () => 0;
+import connectRedis from './connect';
 
-export default fun;
+export const ResourceKey = 'resource/ioredis/redis';
+
+export const DefaultMeta = {};
+
+export const DefaultConfig = {
+  host: process.env.SERVICE_REDIS_HOST,
+  port: process.env.SERVICE_REDIS_PORT,
+  password: process.env.SERVICE_REDIS_PASSWORD,
+};
+
+const connector = (meta = DefaultMeta) => (...configs) => ({
+  key: meta.key || ResourceKey,
+  connect: () => connectRedis(meta)(...configs),
+});
+
+export default connector;
