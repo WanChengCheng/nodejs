281c1cbd945806ded923695029ea1a2370f083a3
Wan Cheng Cheng <chengcheng.st@gmail.com>
2019-03-06 18:49:08 +0800

    update boilerplates.

--- packages/create-node-service/boilerplates/server/backing-services/index.js
+++ packages/create-node-service/boilerplates/server/backing-services/index.js
@@ -6,19 +6,33 @@
 import connectMongodb, {
   DefaultConfig as mongoDefaultConfig,
 } from '@chengchengw/backing-service-mongodb';
+import connectOSS, { DefaultConfig as ossDefaultConfig } from '@chengchengw/backing-service-oss';
 import connectResource from '@chengchengw/backing-service-common/lib/connectResources';
 import register from '@chengchengw/backing-service-common/lib/resourceRegister';
 
+export const mongoConnector = connectMongodb()({ ...mongoDefaultConfig });
+
+export const FlitDefaultOSSKey = 'resource/oss/default-bucket';
+
+export const ossConnector = connectOSS({ key: FlitDefaultOSSKey })({
+  ...ossDefaultConfig,
+});
+
 const connect = ({ logger } = {}) => () => connectResource({
-  connectors: [
-    connectMongodb()({
-      ...mongoDefaultConfig,
-    }),
-  ],
+  connectors: [mongoConnector, ossConnector],
   logger,
   register,
 });
 
-export const resourceRegister = register;
+export const services = register;
+
+export const useService = async (connector) => {
+  const connected = services.service(connector.key);
+  if (!connected) {
+    const resource = await connector.connect();
+    services.register(connector.key, resource);
+  }
+  return services.service(connector.key);
+};
 
 export default connect;
