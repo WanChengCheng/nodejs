d476e96e7dd118f458989792ec94f4c4073099a4
Wan Cheng Cheng <chengcheng.st@gmail.com>
2019-03-06 18:23:13 +0800

    prepublish scripts add to backing-service-oss.

--- packages/backing-service-oss/__tests__/backing-service-oss.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const backingServiceOss = require('..');
-
-describe('@chengchengw/backing-service-oss', () => {
-    it('needs tests');
-});
--- packages/backing-service-oss/src/main.js
+++ packages/backing-service-oss/src/main.js
@@ -4,22 +4,6 @@
  * Author: ChegCheng Wan <chengcheng.st@gmail.com>
  */
 
-import connectOSS from './connect';
+export const fun = () => 0;
 
-export const ResourceKey = 'resource/oss/default';
-
-export const DefaultMeta = {};
-
-export const DefaultConfig = {
-  bucket: process.env.SERVICE_OSS_BUCKET,
-  accessKeyId: process.env.SERVICE_OSS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.SERVICE_OSS_ACCESS_KEY_SECRET,
-  endpoint: process.env.SERVICE_OSS_ENDPOINT,
-};
-
-const connector = (meta = DefaultMeta) => (...config) => ({
-  key: meta.key || ResourceKey,
-  connect: () => connectOSS(meta)(...config),
-});
-
-export default connector;
+export default fun;
