c10035f6be46e05ff3b923f0d416af58f8e4cee8
Wan Cheng Cheng <chengcheng.st@gmail.com>
2019-03-13 17:43:05 +0800

    add axios response handler.

---
+++ packages/node-service/server/express/responseHandler.js
@@ -0,0 +1,14 @@
+/*
+ * File: responseHandler.js
+ * File Created: Wednesday, 13th March 2019 5:38:11 pm
+ * Author: ChegCheng Wan <chengcheng.st@gmail.com>
+ */
+
+const axiosHandler = ({ data, data: { status, ...rest } } = {}) => {
+  if (typeof status !== 'undefined' && status) {
+    throw Object.assign(new Error(rest.message), data);
+  }
+  return data;
+};
+
+export default axiosHandler;
